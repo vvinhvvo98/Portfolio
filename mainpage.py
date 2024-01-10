4bdb7cd9670eaa4272ca60fb0df4e048ea847de6
Vinh Vo <105337753+vvinhvvo98@users.noreply.github.com>
2024-01-09 16:13:30 -0800

    almost done

--- mainpage.py
+++ mainpage.py
@@ -3,62 +3,51 @@
 
     @mainpage
     <pre> <p>
-    @section secs_1     OVERVIEW
-                        aaaaaaaaaaa
+    @section secs_1 OVERVIEW
+    This website showcases the culmination of my efforts as a Mechanical Engineering student at Cal Poly. 
+    It encompasses a diverse range of projects, including personal mechatronics projects, Cal Poly senior projects, and those undertaken during my internships.
+    While the website provides comprehensive details about each project, I encourage you to explore any linked external resources for a deeper understanding. 
+    Additionally, my YouTube channel features videos showcasing these projects in action.
 
-    @section secs_2     MECHATRONIC PROJECTS
-                        <b>ROMI TWO-WHEELED ROBOT</b>
-                        
-                        Website: 
-                        https://vvinhvvo98.github.io/ROMI-Two-Wheels-Robot/
-                        
-                        @image html rommi.png
-                        
-                        
-                        
-                        <b>CARD DEALER ROBOT</b>
-                        
-                        Website: 
-                        https://vvinhvvo98.github.io/Card-Dealer-Robot/
-                        
-                        @image html card.png
+    @section secs_2 MECHATRONIC PROJECTS
+    <b>ROMI TWO-WHEELED ROBOT</b>
+    Website: https://vvinhvvo98.github.io/ROMI-Two-Wheels-Robot/
+    
+    @image html rommi.png
+    
+    
+    <b>CARD DEALER ROBOT</b>
+    Website: https://vvinhvvo98.github.io/Card-Dealer-Robot/
+    
+    @image html card.png
                         
                         
-                        
-    @section secs_3     CAL POLY SENIOR PROJECT
-                        <b>PATH OF LIGHT & SOUND</b>
-                        
-                        Website: 
-                        https://vvinhvvo98.github.io/Card-Dealer-Robot/
-                        
-                        @image html tile.png
+    @section secs_3 CAL POLY SENIOR PROJECT
+    <b>PATH OF LIGHT & SOUND</b>
+    Website: https://vvinhvvo98.github.io/Path-of-Light-and-Sound/
+    
+    @image html tile.png
                             
-                        
-                        
-                        
-                        
-    @section secs_4     INTERNSHIP PROJECTS
-                        <b>HORN HOUSING PRESS FIXTURE</b>
-                        
-                        @image html horn.png
-                        
-                        
-                        
-                        <b>DOOR ASSEMBLY FIXTURE</b>
-                        
-                        @image html door.png
-                        
-                        
-                        
-                        <b>WALL MOUNTING BRACKET</b>
-                                  
-                        @image html wall.png
-                        
-                        
-                        
-                        <b>BUS MOUNTING BRACKET</b>
-                            
-                        @image html bus.png
+    
+    @section secs_4 INTERNSHIP PROJECTS
+    <b>HORN HOUSING PRESS FIXTURE</b>
+    
+    @image html horn.png
+    
+    
+    <b>DOOR ASSEMBLY FIXTURE</b>
+    
+    @image html door.png
+    
+    
+    <b>WALL MOUNTING BRACKET</b>
+              
+    @image html wall.png
+    
+    
+    <b>BUS MOUNTING BRACKET</b>
+        
+    @image html bus.png
      </pre> </p>                  
                         
                         
